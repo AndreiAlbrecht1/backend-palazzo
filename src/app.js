7f1a0983c802554388dcf59daaa26fce0e81fcab
Andrei Albrecht <andreialbrecht@gmail.com>
2025-09-29 23:59:11 -0300

    feat(auth): initial setup of authentication route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { listingsRoutes } from './http/routes/listingsRoutes.js';
+import { usersRoutes } from './http/routes/usersRoutes.js';
 import { handlerError } from './http/middlewares/errorHandler.js';
 
 const app = express();
@@ -10,7 +11,12 @@ app.get('/', async (req, res) => {
   res.status(200).json({ ok: true });
 });
 
+app.use('/', authRoutes);
+
+app.use(authMiddleware);
+
 app.use('/listings', listingsRoutes);
+app.use('/users', usersRoutes);
 app.use(handlerError);
 
 export default app;
