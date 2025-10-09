3f43e069fd66cc7bf9baebeb9f5039399a787744
Andrei Albrecht <andreialbrecht@gmail.com>
2025-10-09 18:30:09 -0300

    fix(routes): protect private routes

--- src/http/routes/listingsRoutes.js
+++ src/http/routes/listingsRoutes.js
@@ -1,12 +1,23 @@
 import { Router } from 'express';
 import ListingsController from '../controllers/ListingsController.js';
 import { upload } from '../middlewares/upload.js';
+import AuthMiddleware from '../middlewares/authMiddleware.js';
 
 const router = new Router();
 router.get('/', ListingsController.getAll);
 router.get('/:id', ListingsController.getById);
-router.post('/', upload.array('images', 10), ListingsController.create);
-router.patch('/:id', upload.array('images', 10), ListingsController.update);
-router.delete('/:id', ListingsController.delete);
+router.post(
+  '/',
+  AuthMiddleware,
+  upload.array('images', 10),
+  ListingsController.create,
+);
+router.patch(
+  '/:id',
+  AuthMiddleware,
+  upload.array('images', 10),
+  ListingsController.update,
+);
+router.delete('/:id', AuthMiddleware, ListingsController.delete);
 
 export { router as listingsRoutes };
